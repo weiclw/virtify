5e79663d5a5bd9828a1316d835f30bfb3e5a9c57
weiclw <92598652+weiclw@users.noreply.github.com>
2024-06-19 15:25:34 -0700

    sock network is working

--- runner/qemu_command.go
+++ runner/qemu_command.go
@@ -1,6 +1,10 @@
 package runner
 
 import (
+    "fmt"
+    "math/rand"
+    "os"
+    "strings"
     "virtify/options"
 )
 
@@ -8,6 +12,15 @@ type qemuOptions struct {
     imagePath string
     drive1Path string
     sockNet string
+}
+
+func genUniqueMac() string {
+    mac := fmt.Sprintf("%02x", 0x2)
+    for i := 1; i < 6; i++ {
+        mac = mac + fmt.Sprintf(":%02x", byte(rand.Intn(256)))
+    }
+
+    return mac
 }
 
 func NewQemuOptions(opt *options.Options) qemuOptions {
@@ -28,10 +41,8 @@ func NewQemuOptions(opt *options.Options) qemuOptions {
     }
 }
 
-func (opts *qemuOptions) getCommand() []string {
-    drive1opt := "if=none,media=disk,id=drive1,file=" + opts.drive1Path
-
-    result := []string{
+func (opts *qemuOptions) genInitialCommand() []string {
+    return []string{
         "/opt/homebrew/bin/qemu-system-aarch64",
         "-L",
         "/Applications/UTM.app/Contents/Resources/qemu",
@@ -45,6 +56,44 @@ func (opts *qemuOptions) getCommand() []string {
         "hvf",
         "-accel",
         "tcg,tb-size=512",
+    }
+}
+
+func (opts *qemuOptions) genNetworkPart() []string {
+    if len(opts.sockNet) == 0 {
+        return []string{}
+    } else if strings.HasPrefix(opts.sockNet, "listen=") {
+        return []string{
+            "-netdev",
+            "user,id=n1",
+            "-netdev",
+            "hubport,hubid=1,netdev=n1,id=h1",
+            "-netdev",
+            "socket,id=n2," + opts.sockNet,
+            "-netdev",
+            "hubport,hubid=1,netdev=n2,id=h2",
+            "-netdev",
+            "hubport,hubid=1,id=h3",
+            "-device",
+            "virtio-net-pci,netdev=h3,mac=" + genUniqueMac(),
+        }
+    } else if strings.HasPrefix(opts.sockNet, "connect=") {
+        return []string{
+            "-netdev",
+            "socket,id=n1," + opts.sockNet,
+            "-device",
+            "virtio-net-pci,netdev=n1,mac=" + genUniqueMac(),
+        }
+    } else {
+        fmt.Println("Incorrect sock_net option: " + opts.sockNet)
+        os.Exit(1)
+        return []string{}
+    }
+}
+
+func (opts *qemuOptions) genRest() []string {
+    drive1opt := "if=none,media=disk,id=drive1,file=" + opts.drive1Path
+    return []string{
         "-drive",
         "if=pflash,format=raw,unit=0,file=/Applications/UTM.app/Contents/Resources/qemu/edk2-aarch64-code.fd,readonly=on",
         "-drive",
@@ -63,16 +112,13 @@ func (opts *qemuOptions) getCommand() []string {
         "-device",
         "virtio-blk-pci,drive=drive1,bootindex=1",
         "-drive",
-        drive1opt}
-
-    if len(opts.sockNet) > 0 {
-        result = append(
-            result,
-            "-netdev",
-            "socket,id=net0," + opts.sockNet,
-            "-device",
-            "virtio-net-pci,netdev=net0")
+        drive1opt,
     }
+}
 
-    return result
+func (opts *qemuOptions) getCommand() []string {
+    cmd := opts.genInitialCommand()
+    cmd = append(cmd, opts.genNetworkPart()...)
+    cmd = append(cmd, opts.genRest()...)
+    return cmd
 }
